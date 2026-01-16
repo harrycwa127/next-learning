598e3397bb6cdb2522bc811cff9bff2069e01a29
Harry Tsang <harrytsang@fdssolutions.com.hk>
2026-01-16 17:29:13 +0800

    update meta data with site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://harry-blog-next-learning.vercel.app',
+  generateRobotsTxt: true,
+};
--- src/configs/siteConfigs.ts
+++ src/configs/siteConfigs.ts
@@ -1,4 +1,4 @@
-const fqdn = 'https://https://harry-blog-next-learning.vercel.app/';
+const fqdn = 'https://harry-blog-next-learning.vercel.app';
 const logoPath = '/logo.png';
 const bannerPath = '/og-image.png';
 
