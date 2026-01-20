71281e234d84c2fa93ad54273cf24b5a0b9c40e8
Harry Tsang <harrytsang@fdssolutions.com.hk>
2026-01-20 17:39:24 +0800

    update change local button

--- src/components/LanguageSwitch.tsx
+++ src/components/LanguageSwitch.tsx
@@ -11,7 +11,7 @@ const LanguageSwitch = () => {
     <Link locale={nextLocale} href={{ pathname, query }}>
       <a
         aria-label="Toggle Language"
-        className="rounded p-2 text-2xl leading-6 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800 sm:p-3"
+        className="rounded p-3 text-lg font-medium text-gray-900 transition-colors hover:bg-gray-100 dark:text-gray-100 dark:hover:bg-gray-800"
       >
         {router.locale === 'en' ? '繁' : 'EN'}
       </a>
