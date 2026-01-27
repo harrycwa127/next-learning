b52b4f8bffd9655638c93dbfa9b46507da99cbac
Harry Tsang <harrytsang@fdssolutions.com.hk>
2026-01-27 17:05:15 +0800

    remove SpeedInsights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyDocument() {
   return (
@@ -8,7 +7,6 @@ function MyDocument() {
       <body className="overflow-x-hidden bg-white text-black antialiased transition-colors dark:bg-gray-900 dark:text-white">
         <Main />
         <NextScript />
-        <SpeedInsights />
       </body>
     </Html>
   );
