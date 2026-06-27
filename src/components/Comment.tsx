6150b7ad0413e0056b055d04de250e96a43ec3df
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2026-06-27 18:23:56 +0800

    fix gicuss theme issues for first entire in iPhone

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,28 +1,38 @@
 import Giscus from '@giscus/react';
 import { useRouter } from 'next/dist/client/router';
 import { useTheme } from 'next-themes';
+import { useState, useEffect } from 'react';
 
 import { giscusConfigs } from '@/configs/giscusConfigs';
 
 const Comment = () => {
-  const { theme } = useTheme();
+  const { resolvedTheme } = useTheme();
   const { locale } = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const giscusTheme = resolvedTheme === 'dark' ? 'transparent_dark' : 'light';
 
   return (
     <div id="comment" className="mx-auto max-w-prose py-6">
-      <Giscus
-        repo={giscusConfigs.repo}
-        repoId={giscusConfigs.repoId}
-        category={giscusConfigs.category}
-        categoryId={giscusConfigs.categoryId}
-        mapping="pathname"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="top"
-        theme={theme === 'dark' ? 'dark' : 'light'}
-        loading="lazy"
-        lang={locale}
-      />
+      {mounted && (
+        <Giscus
+          repo={giscusConfigs.repo}
+          repoId={giscusConfigs.repoId}
+          category={giscusConfigs.category}
+          categoryId={giscusConfigs.categoryId}
+          mapping="pathname"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme={giscusTheme}
+          loading="lazy"
+          lang={locale}
+        />
+      )}
     </div>
   );
 };
