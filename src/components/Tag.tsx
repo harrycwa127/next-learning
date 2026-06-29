3b7b6a67d0eccf33cd5f895e872150a84382bae9
Harry Tsang <harrytsang@fdssolutions.com.hk>
2026-06-29 17:13:57 +0800

    update Tag shrink-0

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function Tag({ children, small }: Props) {
   return (
     <span
-      className={`inline-flex items-center rounded-md font-medium border border-gray-200 bg-gray-50/50 text-gray-600 leading-none dark:border-zinc-700 dark:bg-zinc-800/30 dark:text-zinc-400 ${
+      className={`inline-flex items-center rounded-md font-medium border border-gray-200 bg-gray-50/50 text-gray-600 leading-none dark:border-zinc-700 dark:bg-zinc-800/30 dark:text-zinc-400 shrink-0 ${
         small 
           ? 'text-xs px-1.5 py-0.5'
           : 'text-sm px-2.5 py-1' // 針對 text-sm 微調 py 確保高度比例完美
