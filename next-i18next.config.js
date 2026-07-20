4885011ead288144e1102555348514bad0883991
Harry Tsang <harrytsang@fdssolutions.com.hk>
2026-07-20 16:27:07 +0800

    update i18n next-i18n for path and ns update

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['en', 'zh-TW'],
     defaultLocale: 'zh-TW',
   },
+  localePath: path.resolve('./public/locales'),
+  ns: ['common', 'indexPage', 'aichat'],
+  defaultNS: 'common',
 };
